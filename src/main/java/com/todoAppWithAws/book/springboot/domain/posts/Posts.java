33329407a7bd5da4d150abfe4ba5fcfacf78c41a
Kyle Kim <dkim577@wisc.edu>
2022-09-09 00:12:56 +0900

    Add Posts and repo class

---
+++ src/main/java/com/todoAppWithAws/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.todoAppWithAws.book.springboot.domain.posts;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
