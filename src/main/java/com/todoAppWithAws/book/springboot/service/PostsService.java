9fe2230311ad3232f55bbebfc5c4e16345adbcba
Kyle Kim <dkim577@wisc.edu>
2022-09-09 01:18:46 +0900

    Add service classes

---
+++ src/main/java/com/todoAppWithAws/book/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.todoAppWithAws.book.springboot.service;
+
+import com.todoAppWithAws.book.springboot.domain.posts.PostsRepository;
+import com.todoAppWithAws.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/todoAppWithAws/book/springboot/web/PostApiController.java
@@ -0,0 +1,18 @@
+package com.todoAppWithAws.book.springboot.web;
+
+import com.todoAppWithAws.book.springboot.service.PostsService;
+import com.todoAppWithAws.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
