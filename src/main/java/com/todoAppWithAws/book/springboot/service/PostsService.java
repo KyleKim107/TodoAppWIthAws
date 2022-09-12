75a7d535b0e94c6a9baaa86a304258032227e529
Kyle Kim <dkim577@wisc.edu>
2022-09-12 20:19:18 +0900

    Add findByAllDesc() in IndexController

--- src/main/java/com/todoAppWithAws/book/springboot/service/PostsService.java
+++ src/main/java/com/todoAppWithAws/book/springboot/service/PostsService.java
@@ -2,6 +2,7 @@ package com.todoAppWithAws.book.springboot.service;
 
 import com.todoAppWithAws.book.springboot.domain.posts.Posts;
 import com.todoAppWithAws.book.springboot.domain.posts.PostsRepository;
+import com.todoAppWithAws.book.springboot.web.dto.PostsListResponseDto;
 import com.todoAppWithAws.book.springboot.web.dto.PostsResponseDto;
 import com.todoAppWithAws.book.springboot.web.dto.PostsSaveRequestDto;
 import com.todoAppWithAws.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -37,5 +40,11 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/todoAppWithAws/book/springboot/web/IndexController.java
+++ src/main/java/com/todoAppWithAws/book/springboot/web/IndexController.java
@@ -1,12 +1,23 @@
 package com.todoAppWithAws.book.springboot.web;
 
+import com.todoAppWithAws.book.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
