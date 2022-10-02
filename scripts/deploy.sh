1af22678b02580eeb03b477d62d35d7811be848e
Kyle Kim <dkim577@wisc.edu>
2022-10-02 21:22:46 +0900

    re-deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,9 +9,10 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME}.*.jar | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
