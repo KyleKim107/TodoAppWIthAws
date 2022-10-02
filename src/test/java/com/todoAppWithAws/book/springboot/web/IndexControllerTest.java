69028a9716843369303e03e8c32a6a8db44c2cdd
Kyle Kim <dkim577@wisc.edu>
2022-10-02 16:07:11 +0900

    modify index h1 and test condition

--- src/test/java/com/todoAppWithAws/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/todoAppWithAws/book/springboot/web/IndexControllerTest.java
@@ -22,6 +22,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        Assertions.assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+        Assertions.assertThat(body).contains("생일축하해~!");
     }
 }
