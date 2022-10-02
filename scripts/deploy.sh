2988d59710ecec0f42594fddc02692ceccc92d62
Kyle Kim <dkim577@wisc.edu>
2022-10-02 21:39:35 +0900

    modify deploy.sh to kill 8080 port

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=TodoAppWIthAws
+PROJECT_NAME=TodoAppWithAws
 
 echo "> Build 파일 복사"
 
@@ -9,7 +9,8 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+#CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(lsof -t -i:8080)
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
