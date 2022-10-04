2910adbfdab8140ffbd1222d0aea97cb05ee8650
Kyle Kim <dkim577@wisc.edu>
2022-10-04 23:15:35 +0900

    modify stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,7 +8,7 @@ source ${ABSDIR}/profile.sh # like import in java
 IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+IDLE_PID=$(lsof -t -i:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
